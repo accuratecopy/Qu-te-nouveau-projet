352ed250f157c911344d9d3f8dbb2ec9d5fae4b2
Anthony Jonas <anthony.jonas@outlook.com>
2018-11-12 14:24:09 +0100

    Finalisation quête symfony param converter

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/article", name="article")
+     */
+    public function index()
+    {
+        return $this->render('article/index.html.twig', [
+            'controller_name' => 'ArticleController',
+        ]);
+    }
+
+    /**
+     * @Route("/article/{id}", name="article_show")
+     */
+    public function show(Article $article) :Response
+    {
+        return $this->render('article/show.html.twig', [
+            'article'=>$article
+        ]);
+    }
+
+}
---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategoryController extends AbstractController
+{
+    /**
+     * @Route("/category", name="category")
+     */
+    public function index()
+    {
+        return $this->render('category/index.html.twig', [
+            'controller_name' => 'CategoryController',
+        ]);
+    }
+
+    /**
+     * @Route("/category/{id}", name="category_show")
+     */
+    public function show(Category $category) :Response
+    {
+        return $this->render('category/show.html.twig', [
+            'category'=>$category
+        ]);
+    }
+}
